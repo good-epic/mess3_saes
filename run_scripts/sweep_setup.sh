2ef448e2daed6589a161e2daccea50a887ba9a61
good-epic <good.epic@gmail.com>
2025-10-16 16:34:55 -0700

    sweept_setup.sh for remote pods

---
+++ run_scripts/sweep_setup.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+apt update
+apt install rsync
+
+mkdir -p /workspace/outputs/
+mkdir -p /workspace/outputs/saes
+mkdir -p /workspace/outputs/saes/multipartite_003e
+mkdir -p /workspace/outputs/reports/
+mkdir -p /workspace/outputs/reports/multipartite_003e
+mkdir -p /workspace/outputs/checkpoints/
+mkdir -p /workspace/outputs/checkpoints/multipartite_003
